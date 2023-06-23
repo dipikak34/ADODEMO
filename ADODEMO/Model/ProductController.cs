726d3bd1dfb698af7365980c1bae543c40288665
dipikak34 <dipikak34@users.noreply.github.com>
2023-06-23 15:41:29 +0530

    Add project files.

---
+++ ADODEMO/Model/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ADODEMO.Model
+{
+
+    [ApiController]
+
+    public class ProductController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductController(IConfiguration confi)
+        {
+            _configuration = confi;
+        }
+
+        [HttpGet]
+        [Route("GetAllProduct")]
+        public List<ProductModel> GetAllProduct()
+        {
+            
+            List<ProductModel> Lst = new List<ProductModel>();
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            SqlCommand cmd = new SqlCommand("select * from Office_Employee", con);
+            SqlDataAdapter da=new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            for (int i = 0; i<dt.Rows.Count;i++)
+            {
+                ProductModel obj = new ProductModel();
+                obj.ID = int.Parse(dt.Rows[i][""].ToString());
+                obj.Salary = double.Parse(dt.Rows[i]["Salary"].ToString());
+                obj.Name= (dt.Rows[i][""].ToString());
+                obj.Last_Name= (dt.Rows[i][""].ToString());
+                obj .Mobile_No= int.Parse(dt.Rows[i][""].ToString());
+
+                Lst.Add(obj);
+            }
+
+
+            return Lst;
+        }
+    }
+}
+
+
+//        // GET: api/<ProductController>
+//        [HttpGet]
+//        public IEnumerable<string> Get()
+//        {
+//            return new string[] { "value1", "value2" };
+//        }
+
+//        // GET api/<ProductController>/5
+//        [HttpGet("{id}")]
+//        public string Get(int id)
+//        {
+//            return "value";
+//        }
+
+//        // POST api/<ProductController>
+//        [HttpPost]
+//        public void Post([FromBody] string value)
+//        {
+//        }
+
+//        // PUT api/<ProductController>/5
+//        [HttpPut("{id}")]
+//        public void Put(int id, [FromBody] string value)
+//        {
+//        }
+
+//        // DELETE api/<ProductController>/5
+//        [HttpDelete("{id}")]
+//        public void Delete(int id)
+//        {
+//        }
+//    }
+//}
---
+++ ADODEMO/Model/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ADODEMO.Model
+{
+    public class ProductModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Last_Name { get; set; }
+        public double Salary { get; set; }
+        public int Mobile_No { get; set; }
+    }
+}
